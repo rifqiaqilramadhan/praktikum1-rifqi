7aa1e63423f49ce3ec96c04716ce8783dfca68bd
rifqiaqilramadhan <145142128+rifqiaqilramadhan@users.noreply.github.com>
2023-09-27 23:46:56 +0700

    Add files via upload

---
+++ menhitung luas (rifqiaqilramadhan).py
@@ -0,0 +1,6 @@
+panjang = float(input("masukkan panjang ruangan:") )
+lebar = float(input("masukkan lebar ruangan :"))
+satuan =  input("masukkan meter/inci :")
+luas = panjang*lebar 
+
+print ("luas ruangan yaitu ", luas, satuan)
